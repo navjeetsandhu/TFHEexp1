db0eda4de2718c11a52b534c695a88bbc4dc4e3f
Navjeet Sandhu <navjeet.sandhu@gmail.com>
2024-12-20 09:39:54 -0500

    changes

--- unit_test/externalproduct/externalproductbatch.cpp
+++ unit_test/externalproduct/externalproductbatch.cpp
@@ -12,9 +12,8 @@ int main()
     default_random_engine engine(seed_gen());
     uniform_int_distribution<uint32_t> binary(0, 1);
     constexpr int batch = 2;
-    cout << "test p=1" << endl;
+    cout << "test p=1: lvl1 batch" << endl;
 
-    cout << "lvl1 batch " << batch << endl;
     {
         lweKey key;
         BooleanArrayn<lvl1param::n, batch> p;
@@ -53,9 +52,46 @@ int main()
             }
     }
     cout << "Passed" << endl;
+    cout << "test p=-1: lvl1 batch" << endl;
+
+    {
+        lweKey key;
+        BooleanArrayn<lvl1param::n, batch> p;
+
+        for (int j = 0; j < batch; j++)
+            for (int i = 0; i < lvl1param::n; i++)
+                p[j][i] = (binary(engine) > 0);
+
+        Polynomialn<lvl1param, batch> pmu;
+        for (int j = 0; j < batch; j++)
+            for (int i = 0; i < lvl1param::n; i++)
+                pmu[j][i] = p[j][i] ? lvl1param::mu : -lvl1param::mu;
+
+        TRLWEn<lvl1param, batch> c = trlweSymEncryptbatch<lvl1param, batch>(pmu, key.lvl1);
+
+        Polynomialn<TFHEpp::lvl1param, batch> plainpoly = {
+            static_cast<typename lvl1param::T>(0)};
+
+        for (int j = 0; j < batch; j++)
+            plainpoly[j][0] = -1;
+
+        //for (int j = 0; j < batch; j++)
+        //    for (int i = 0; i < lvl1param::n; i++)
+        //       cout << j << " " << i << " " << plainpoly[j][i] <<  endl;
+
+        TRGSWFFTn<lvl1param, batch> trgswfft =
+            trgswfftSymEncryptbatch<lvl1param, batch>(plainpoly, key.lvl1);
+        trgswfftExternalProductbatch<lvl1param, batch>(c, c, trgswfft);
 
 
-
+        BooleanArrayn<lvl1param::n, batch> p2 = trlweSymDecryptbatch<lvl1param, batch>(c, key.lvl1);
+        for (int j = 0; j < batch; j++)
+            for (int i = 0; i < lvl1param::n; i++) {
+                cout << j << " " << i << " " << p[j][i] << "  " << p2[j][i] << endl;
+                c_assert(p[j][i] == !p2[j][i]);
+            }
+    }
+    cout << "Passed" << endl;
 
 
 }
