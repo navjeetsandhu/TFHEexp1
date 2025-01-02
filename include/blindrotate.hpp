0f8b9d27f37316b6b0129ac185370044aed24d6d
Navjeet Sandhu <navjeet.sandhu@gmail.com>
2025-01-02 11:54:10 -0500

    changes

--- include/blindrotate.hpp
+++ include/blindrotate.hpp
@@ -42,4 +42,42 @@ void BlindRotate(TRLWE<typename P::targetP> &res,
         CMUXFFTwithPolynomialMulByXaiMinusOne<P>(res, bkfft[i], aLong);
     }
 }
+
+
+template <class P, int batch, uint32_t num_out = 1>
+void BlindRotatebatch(TRLWEn<typename P::targetP, batch> &res,
+                 const TLWEn<typename P::domainP, batch> &tlwe,
+                 const BootstrappingKeyFFT<P> &bkfft,
+                 const Polynomialn<typename P::targetP, batch> &testvector)
+{
+    res = {};
+    for (int j = 0; j < batch; j++) {
+        constexpr uint32_t bitwidth = bits_needed<num_out - 1>();
+        const uint32_t bLong =
+            2 * P::targetP::n -
+            ((tlwe[j][P::domainP::k * P::domainP::n] >>
+              (std::numeric_limits<typename P::domainP::T>::digits - 1 -
+               P::targetP::nbit + bitwidth))
+             << bitwidth);
+
+        PolynomialMulByXai<typename P::targetP>(res[j][P::targetP::k], testvector[j],
+                                                bLong);
+    }
+    for (int i = 0; i < P::domainP::k * P::domainP::n; i++) {
+        intArray<batch> aLongArray;
+        for (int j = 0; j < batch; j++) {
+            constexpr typename P::domainP::T roundoffset =
+                1ULL << (std::numeric_limits<typename P::domainP::T>::digits -
+                         2 - P::targetP::nbit + bitwidth);
+            aLongArray[j] =
+                (tlwe[j][i] + roundoffset) >>
+                (std::numeric_limits<typename P::domainP::T>::digits - 1 -
+                 P::targetP::nbit + bitwidth)
+                    << bitwidth;
+        }
+        // Do not use CMUXFFT to avoid unnecessary copy.
+        CMUXFFTwithPolynomialMulByXaiMinusOnebatch<P, batch>(res, bkfft[i], aLongArray);
+    }
+}
+
 }  // namespace TFHEpp
